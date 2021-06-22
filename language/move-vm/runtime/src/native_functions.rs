a4974fdb938f2976bc7013cb43dd87cf82123c47
Dm. Yakushev <rig410@yandex.ru>
2021-06-22 19:59:07 +0300

    Added u256 type.

--- language/move-vm/runtime/src/native_functions.rs
+++ language/move-vm/runtime/src/native_functions.rs
@@ -6,7 +6,7 @@ use move_core_types::{
     account_address::AccountAddress, gas_schedule::CostTable, language_storage::CORE_CODE_ADDRESS,
     value::MoveTypeLayout, vm_status::StatusType,
 };
-use move_vm_natives::{account, bcs, debug, event, hash, signature, signer, vector};
+use move_vm_natives::{account, bcs, debug, hash, signature, signer, u256, vector, event};
 use move_vm_types::{
     data_store::DataStore,
     gas_schedule::CostStrategy,
@@ -44,6 +44,17 @@ pub(crate) enum NativeFunction {
     SignerBorrowAddress,
     CreateSigner,
     DestroySigner,
+
+    U256FromU8,
+    U256FromU64,
+    U256FromU128,
+    U256AsU8,
+    U256AsU64,
+    U256AsU128,
+    U256Mul,
+    U256Div,
+    U256Sub,
+    U256Add,
 }
 
 impl NativeFunction {
@@ -75,6 +86,18 @@ impl NativeFunction {
             (&CORE_CODE_ADDRESS, "Debug", "print") => DebugPrint,
             (&CORE_CODE_ADDRESS, "Debug", "print_stack_trace") => DebugPrintStackTrace,
             (&CORE_CODE_ADDRESS, "Signer", "borrow_address") => SignerBorrowAddress,
+
+            (&CORE_CODE_ADDRESS, "U256", "from_u8") => U256FromU8,
+            (&CORE_CODE_ADDRESS, "U256", "from_u64") => U256FromU64,
+            (&CORE_CODE_ADDRESS, "U256", "from_u128") => U256FromU128,
+            (&CORE_CODE_ADDRESS, "U256", "as_u8") => U256AsU8,
+            (&CORE_CODE_ADDRESS, "U256", "as_u64") => U256AsU64,
+            (&CORE_CODE_ADDRESS, "U256", "as_u128") => U256AsU128,
+
+            (&CORE_CODE_ADDRESS, "U256", "mul") => U256Mul,
+            (&CORE_CODE_ADDRESS, "U256", "div") => U256Div,
+            (&CORE_CODE_ADDRESS, "U256", "sub") => U256Sub,
+            (&CORE_CODE_ADDRESS, "U256", "add") => U256Add,
             _ => return None,
         })
     }
@@ -107,11 +130,21 @@ impl NativeFunction {
             Self::SignerBorrowAddress => signer::native_borrow_address(ctx, t, v),
             Self::CreateSigner => account::native_create_signer(ctx, t, v),
             Self::DestroySigner => account::native_destroy_signer(ctx, t, v),
+
+            // u256
+            Self::U256FromU8 => u256::from_u8(ctx, t, v),
+            Self::U256FromU64 => u256::from_u64(ctx, t, v),
+            Self::U256FromU128 => u256::from_u128(ctx, t, v),
+
+            Self::U256AsU8 => u256::as_u8(ctx, t, v),
+            Self::U256AsU64 => u256::as_u64(ctx, t, v),
+            Self::U256AsU128 => u256::as_u128(ctx, t, v),
+
+            Self::U256Mul => u256::mul(ctx, t, v),
+            Self::U256Div => u256::div(ctx, t, v),
+            Self::U256Sub => u256::sub(ctx, t, v),
+            Self::U256Add => u256::add(ctx, t, v),
         };
-        debug_assert!(match &result {
-            Err(e) => e.major_status().status_type() == StatusType::InvariantViolation,
-            Ok(_) => true,
-        });
         result
     }
 }
